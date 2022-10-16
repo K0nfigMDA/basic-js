0e7ad1bd6787c08e0a19343cfce7d1765316965b
K0nfigMDA <iamjustadreamer@yandex.ru>
2022-10-16 17:39:00 +0300

    feat: complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -12,12 +12,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * repeater('STRING', { repeatTimes: 3, separator: '**', 
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
- * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * => STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
+ * 	
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function repeater(str, options) {
+	const string = `${str}`;
+	const repeatTimes = options.hasOwnProperty('repeatTimes') ? options.repeatTimes : 1;
+	const separator = options.hasOwnProperty('separator') ? options.separator : '+';
+	const addition = options.hasOwnProperty('addition') ? (`${options.addition}` ) : '';
+	const additionRepeatTimes = options.hasOwnProperty('additionRepeatTimes') ? options.additionRepeatTimes : 1;
+	const additionSeparator = options.hasOwnProperty('additionSeparator') ? options.additionSeparator : '|';
+
+	function addSeparator(string, separator, times) {
+		let result = string
+		for(let i = 0; i < times - 1; i++) {
+			result += separator + string
+		}
+		return result
+	}
+
+	const additions = addSeparator(addition, additionSeparator, additionRepeatTimes)
+	const strings = addSeparator(string + additions, separator, repeatTimes)
+
+
+	return strings 
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+ function transform(arr) {
+	if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!")
+	let cloneArr = arr.slice()
+	
+	const options = ['--discard-prev', '--discard-next' , '--double-next' , '--double-prev']
+	
+	for(let i = 0; i < cloneArr.length; i++) {
+		
+		if(cloneArr[i] === '--discard-next') {
+			if(!options.includes(cloneArr[i+1])) {cloneArr.splice(i, 2)}
+			else {cloneArr.splice(i,1)}
+			
+		}
+		if(cloneArr[i] === '--double-next') {
+			if(cloneArr[i+1] && !options.includes(cloneArr[i+1])) {cloneArr.splice(i, 1, cloneArr[i+1])}
+			else {cloneArr.splice(i, 1)}
+			
+			
+		}
+		if(cloneArr[i] === '--double-prev') {
+			const arrIndex1 = arr.findIndex(el => el === '--double-prev')
+			if(cloneArr[i-1] && !options.includes(cloneArr[i-1]) && arr[arrIndex1-2] !== '--discard-next') {cloneArr.splice(i, 1, cloneArr[i-1])}
+			else {cloneArr.splice(i, 1)}
+			
+		}
+
+		if(cloneArr[i] === '--discard-prev') {
+			const arrIndex = arr.findIndex(el => el === '--discard-prev')
+			
+			if(cloneArr[i-1] && !options.includes(cloneArr[i-1]) && arr[arrIndex-2] !== '--discard-next') {cloneArr.splice(i-1, 2)}
+			else {cloneArr.splice(i,1)}
+			
+		}
+		
+	}
+	return cloneArr
 }
+
+
+
+
+
 
 module.exports = {
   transform
